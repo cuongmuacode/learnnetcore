89bbffca0d439c64faeba7726b0fe8397a042fa8
Lưu Mạnh Cường <cuonglmsinx@gmail.com>
2025-04-02 10:23:32 +0700

    project init

---
+++ LearnNetCore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnNetCore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
